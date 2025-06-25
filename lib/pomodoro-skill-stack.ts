a4568daece0441ea0d60b2b4df1090ab6ce0d4f7
yunohana <yu.hanaoka+github@gmail.com>
2025-06-25 23:20:06 +0900

    [add]prototype skill

--- lib/pomodoro-skill-stack.ts
+++ lib/pomodoro-skill-stack.ts
@@ -1,16 +1,151 @@
-import * as cdk from 'aws-cdk-lib';
+import {
+  CfnOutput, Duration, RemovalPolicy, Stack, StackProps,
+  aws_iam as iam,
+  aws_lambda as lambda,
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class PomodoroSkillStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class RootStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const
+      bucket = new s3.Bucket(this, 'Bucket', {
+        bucketName: `pomodoro-${this.account}`,
+        versioned: false,
+        publicReadAccess: false,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+      }),
+      role = new iam.Role(this, 'Role', {
+        roleName: `pomodoro-${this.account}`,
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        ],
+      });
+    bucket.grantRead(role);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PomodoroSkillQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new lambda.Function(this, 'Function', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        const AWS = require('aws-sdk');
+        const s3 = new AWS.S3();
+
+        exports.handler = async (event) => {
+          console.log('Request:', JSON.stringify(event, null, 2));
+
+          const requestType = event.request.type;
+          const intentName = event.request.intent?.name;
+
+          if (requestType === 'LaunchRequest' || intentName === 'StartPomodoroIntent') {
+            try {
+              // List MP4 files from S3 bucket
+              const bucketName = process.env.BUCKET_NAME;
+              const listParams = {
+                Bucket: bucketName,
+                Prefix: '',
+              };
+
+              const objects = await s3.listObjectsV2(listParams).promise();
+              const mp4Files = objects.Contents
+                .filter(obj => obj.Key.toLowerCase().endsWith('.mp4'))
+                .sort((a, b) => a.Key.localeCompare(b.Key))
+                .map(obj => obj.Key);
+
+              if (mp4Files.length === 0) {
+                return {
+                  version: '1.0',
+                  response: {
+                    outputSpeech: {
+                      type: 'PlainText',
+                      text: 'ポモドーロ用の動画ファイルが見つかりません。',
+                    },
+                    shouldEndSession: true,
+                  },
+                };
+              }
+
+              // Generate presigned URLs for MP4 files
+              const videoUrls = await Promise.all(
+                mp4Files.map(async (key) => {
+                  const url = await s3.getSignedUrlPromise('getObject', {
+                    Bucket: bucketName,
+                    Key: key,
+                    Expires: 3600, // 1 hour
+                  });
+                  return url;
+                })
+              );
+
+              return {
+                version: '1.0',
+                response: {
+                  outputSpeech: {
+                    type: 'PlainText',
+                    text: 'ポモドーロを開始します。',
+                  },
+                  directives: [
+                    {
+                      type: 'VideoApp.Launch',
+                      videoItem: {
+                        source: videoUrls[0],
+                        metadata: {
+                          title: 'ポモドーロタイマー',
+                          subtitle: 'Focus Time',
+                        },
+                      },
+                    },
+                  ],
+                  shouldEndSession: true,
+                },
+              };
+            } catch (error) {
+              console.error('Error:', error);
+              return {
+                version: '1.0',
+                response: {
+                  outputSpeech: {
+                    type: 'PlainText',
+                    text: 'エラーが発生しました。もう一度お試しください。',
+                  },
+                  shouldEndSession: true,
+                },
+              };
+            }
+          }
+
+          return {
+            version: '1.0',
+            response: {
+              outputSpeech: {
+                type: 'PlainText',
+                text: 'こんにちは。ポモドーロと言ってください。',
+              },
+              shouldEndSession: false,
+            },
+          };
+        };
+      `),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+      role: role,
+      timeout: Duration.seconds(30),
+    });
+
+    new CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+      description: 'S3 bucket name for MP4 files',
+    });
+    new CfnOutput(this, 'AlexaSkillFunctionArn', {
+      value: handler.functionArn,
+      description: 'Lambda function ARN for Alexa skill',
+    });
   }
 }
+
--- test/pomodoro-skill.test.ts
+++ test/pomodoro-skill.test.ts
@@ -1,17 +1,32 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as PomodoroSkill from '../lib/pomodoro-skill-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as PomodoroSkill from '../lib/pomodoro-skill-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/pomodoro-skill-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new PomodoroSkill.PomodoroSkillStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('S3 Bucket Created', () => {
+  const app = new cdk.App();
+  const stack = new PomodoroSkill.PomodoroSkillStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    BucketEncryption: {
+      ServerSideEncryptionConfiguration: [
+        {
+          ServerSideEncryptionByDefault: {
+            SSEAlgorithm: 'AES256'
+          }
+        }
+      ]
+    }
+  });
+});
+
+test('Lambda Function Created', () => {
+  const app = new cdk.App();
+  const stack = new PomodoroSkill.PomodoroSkillStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Runtime: 'nodejs18.x',
+    Handler: 'index.handler'
+  });
 });
