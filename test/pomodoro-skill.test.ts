8c912e81b8d85c68912b0ac94b11b759be46eb38
yunohana <yu.hanaoka+github@gmail.com>
2025-06-25 22:19:41 +0900

    initial commit

---
+++ test/pomodoro-skill.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PomodoroSkill from '../lib/pomodoro-skill-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pomodoro-skill-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PomodoroSkill.PomodoroSkillStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
